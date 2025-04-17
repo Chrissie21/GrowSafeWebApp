b98b6040b85ef061ad62a1e410919c1baeca248e
Chris <chrisbalele21@gmail.com>
2025-04-17 10:04:03 +0300

    Updated static files directory.

--- growsafedjango/growsafedjango/settings.py
+++ growsafedjango/growsafedjango/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Directory for static files
+STATIC_ROOT = BASE_DIR / "staticfiles"    # Directory where collectstatic will copy files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
