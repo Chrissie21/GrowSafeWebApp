7ff7be41e70f473f7da2718545f5b163dbb025a4
Chris <chrisbalele21@gmail.com>
2025-04-19 03:04:22 +0300

    Fixed routing

--- reactadmin/src/App.js
+++ reactadmin/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Sidebar from "./components/Sidebar";
-import Login from "./components/Login";
-import AdminPanel from "./components/AdminPanel";
+import Sidebar from "./components /Sidebar";
+import Login from "./components /Login";
+import AdminPanel from "./components /AdminPanel";
 
 const apiBaseUrl = "http://localhost:8000/api/auth/";
 
