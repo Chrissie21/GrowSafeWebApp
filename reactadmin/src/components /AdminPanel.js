815f9d1e170eea6625effc39e464515eabd43bc4
Chris <chrisbalele21@gmail.com>
2025-04-19 03:23:24 +0300

    Updated to include the new Users and InvestmentOptions components

--- reactadmin/src/components /AdminPanel.js
+++ reactadmin/src/components /AdminPanel.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Dashboard from "./Dashboard";
 import Transactions from "./Transactions";
 import UpdateMobile from "./UpdateMobile";
+import Users from "./Users";
+import InvestmentOptions from "./InvestmentOptions";
 
 function AdminPanel() {
   const [activeSection, setActiveSection] = useState("dashboard");
@@ -21,6 +23,8 @@ function AdminPanel() {
       {activeSection === "dashboard" && <Dashboard />}
       {activeSection === "transactions" && <Transactions />}
       {activeSection === "mobile" && <UpdateMobile />}
+      {activeSection === "users" && <Users />}
+      {activeSection === "investment-options" && <InvestmentOptions />}
     </div>
   );
 }
