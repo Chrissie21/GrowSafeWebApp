e5aa8105a911f04d828bfb0fa5d1b47052cbb987
Chris <chrisbalele21@gmail.com>
2025-04-20 20:43:21 +0300

    Commented out path for testing

--- growsafedjango/accounts/urls.py
+++ growsafedjango/accounts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView
 app_name = 'api-auth'
 
 urlpatterns = [
-    path('login//', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    # path('login//', LoginView.as_view(template_name='registration/login.html'), name='login'),
     # Client endpoints
     path('', views.auth_root, name='auth_root'),
     path('signup/', views.signup, name='signup'),
