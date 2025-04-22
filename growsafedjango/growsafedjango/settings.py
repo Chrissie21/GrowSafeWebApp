b778fa3636127b33604766623e050b27ffe2937d
Chris <chrisbalele21@gmail.com>
2025-04-22 08:24:56 +0300

    Settings tweak

--- growsafedjango/growsafedjango/settings.py
+++ growsafedjango/growsafedjango/settings.py
@@ -132,7 +132,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
     "http://localhost:55078",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001"
 ]
 
 # REST Framework settings
