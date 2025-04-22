d5a58f7e501d16e28408f5dc7348bc1fa73280de
Chris <chrisbalele21@gmail.com>
2025-04-22 08:36:17 +0300

    Axios instance isolation

---
+++ reactadmin/src/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api/auth/',
+});
+
+// Add request interceptor to attach the token
+api.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem('access_token') || sessionStorage.getItem('access_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
